9bdc533024bf17c2d189b6b098ef9cf5642dc5c2
Thiago Locatelli <thiago.locatelli@gmail.com>
2014-04-13 20:31:09 -0400

    Removing unused methods

--- src/main/java/com/github/thiagolocatelli/pinpayments/model/Transfer.java
+++ src/main/java/com/github/thiagolocatelli/pinpayments/model/Transfer.java
@@ -1,5 +1,35 @@
 package com.github.thiagolocatelli.pinpayments.model;
 
+import java.util.Date;
+
 public class Transfer {
+
+	String state;
+	Date paidAt;
+	String token;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Date getPaidAt() {
+		return paidAt;
+	}
+
+	public void setPaidAt(Date paidAt) {
+		this.paidAt = paidAt;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
 
 }
